6db405ddaf1aaabf2b21cc978d00e545c02fd7f2
Davidchhing <davidhchhing@gmail.com>
2018-09-30 08:15:50 -0700

    created routed.js file and added pathname to server file

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 var http = require("http");
+var url = require('url');
 
 function start() {
 
     function onRequest(request, response) {
-        console.log('request received.');
+        var pathname = url.parse(request.url).pathname;
+        console.log('Request for' + pathname + 'received.');
         response.writeHead(200, {'Content-Type": "text/plain'});
         response.write("Hello Alchemy and world");
         response.end();
     }
 
-    http.createServer(onRequest).listen(1234);
+    http.createServer(onRequest).listen(8888);
     console.log('Server has started.');
 }
 
 exports.start = start;
-// start page 23/54 //
+
+

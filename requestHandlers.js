a735d6df3487e2adead2be65b6f76ce54d909b6e
Davidchhing <davidhchhing@gmail.com>
2018-09-30 11:17:09 -0700

    rewrote image file path

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,38 +1,55 @@
 var querystring = require('querystring'),
     fs = require('fs');
+var formidable = require('formidable');
 
-function start(response, postData) {
+function start(response) {
     console.log('request for handler "start" was called.');
     
     var body = '<html>' +
-        '<head>' +
-        '<meta http-equiv="Content-Type" content="text/html; ' +
-        'charset=UTF-8" />' +
-        '</head>' +
-        '<body>' +
-        '<form action="/upload" method="post">' +
-        '<textarea name="text" rows="20" cols="60"></textarea>' +
-        '<input type="submit" value="Submit text" />' +
-        '</form>' +
-        '</body>' +
-        '</html>';
+    '<head>' +
+     '<meta http-equiv="Content-Type" ' +
+     'content="text/html; charset=UTF-8" />' +
+     '</head>' +
+     '<body>' +
+     '<form action="/upload" enctype="multipart/form-data" ' +
+     'method="post">' +
+     '<input type="file" name="upload" multiple="multiple">' +
+     '<input type="submit" value="Upload file" />' +
+     '</form>' +
+     '</body>' +
+     '</html>';
 
     response.writeHead(200, { 'content-Type': 'text/html' });
     response.write(body);
     response.end();
 }
 
-function upload(response, postData) {
+function upload(response, request) {
     console.log('Request handler "upload" was called.');
-    response.writeHead(200, { 'content-Type': 'text/plain' });
-    response.write('you have sent: ' + querystring.parse(postData).text);
-    response.end();
+
+    var form = new formidable.IncomingForm();
+    console.log('about to parse');
+    form.parse(request, function(error, fields, files) {
+        console.log('parsing done');
+
+        fs.rename(files.upload.path, '../pics/mountains.png', function(error) {
+            if(error) {
+                fs.unlink('../pics/mountains.png');
+                fs.rename(files.upload.path, '../pics/mountains.png');
+            }
+        });
+
+        response.writeHead(200, { 'content-Type': 'text/html' });
+        response.write('received image:<br/>');
+        response.write('<img src="/show" />');
+        response.end();
+    }); 
 }
 
 function show(response) {
     console.log('Request handler "show" was called.');
     response.writeHead(200, { 'Content-Type': 'image/png' });
-    fs.createReadStream('/tmp/test.png').pipe(response);
+    fs.createReadStream('../pics/mountains.png').pipe(response);
 }
 
 exports.start = start;

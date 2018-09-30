15f74a747ba351e106b3c40548bf5c42eecaa467
Davidchhing <davidhchhing@gmail.com>
2018-09-29 18:30:55 -0700

    created index.js page and start module

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 var http = require("http");
 
-function onRequest(request, response) {
-    console.log("request received.");
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello Alchemy and world");
-    response.end();
+function start() {
+
+    function onRequest(request, response) {
+        console.log('request received.');
+        response.writeHead(200, {'Content-Type": "text/plain'});
+        response.write("Hello Alchemy and world");
+        response.end();
+    }
+
+    http.createServer(onRequest).listen(1234);
+    console.log('Server has started.');
 }
 
-http.createServer(onRequest).listen(1234);
-
-console.log("Server has started.");
+exports.start = start;
+// start page 23/54 //

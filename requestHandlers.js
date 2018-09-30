b5cdb59bc3f42f20dea4de856fba162fe1f39490
Davidchhing <davidhchhing@gmail.com>
2018-09-30 10:01:27 -0700

    created text field in web server

--- requestHandlers.js
+++ requestHandlers.js
@@ -2,13 +2,23 @@ var exec = require('child_process').exec;
 
 function start(response) {
     console.log('request for handler "start" was called.');
-  
+    
+    var body = '<html>' +
+        '<head>' +
+        '<meta http-equiv="Content-Type" content="text/html; ' +
+        'charset=UTF-8" />' +
+        '</head>' +
+        '<body>' +
+        '<form action="/upload" method="post">' +
+        '<textarea name="text" rows="20" cols="60"></textarea>' +
+        '<input type="submit" value="Submit text" />' +
+        '</form>' +
+        '</body>' +
+        '</html>';
 
-    exec('ls -lah', function(error, stdout, stderr) {
-        response.writeHead(200, { 'content-Type': 'text/plain' });
-        response.write(stdout);
-        response.end();
-    });
+    response.writeHead(200, { 'content-Type': 'text/html' });
+    response.write(body);
+    response.end();
 }
 
 function upload(response) {

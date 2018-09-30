12ba26f40ede7e0004753f7c0c1cf2aef3f3867a
Davidchhing <davidhchhing@gmail.com>
2018-09-30 09:21:02 -0700

    added 10 second timer to start/upload page

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,18 @@
-function(start) {
+function start() {
     console.log('request for handler "start" was called.');
+
+    function sleep(milliSeconds) {
+        let startTime = new Date().getTime();
+        while(new Date().getTime() < startTime + milliSeconds);
+    }
+
+    sleep(10000);
+    return 'hello start';
 }
 
 function upload() {
     console.log('Request handler "upload" was called.');
+    return 'hello upload';
 }
 
 exports.start = start;
